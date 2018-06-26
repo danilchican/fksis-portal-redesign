bb1b4d3f2112e66764873188114fbb49252444ab
danilchican <danilchican@mail.ru>
2018-06-26 17:56:07 +0300

    Scripts task

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@ const gulp = require('gulp'),
     cleancss = require('gulp-clean-css'),
     notify = require('gulp-notify'),
     rename = require('gulp-rename'),
-    sass = require('gulp-ruby-sass');
+    sass = require('gulp-ruby-sass'),
+    uglify = require('gulp-uglify');
 
 gulp.task('styles', function () {
-    return sass('src/scss/main.scss', {style: 'expanded'})
+    return sass('src/scss/**/*.scss', {style: 'expanded'})
         .pipe(autoprefixer('last 3 versions'))
         .pipe(gulp.dest('build/css'))
         .pipe(rename({suffix: '.min'}))
@@ -15,6 +16,15 @@ gulp.task('styles', function () {
         .pipe(notify({message: 'Styles task complete'}));
 });
 
+gulp.task('scripts', function () {
+   return gulp.src('./src/js/**/*.js')
+       .pipe(gulp.dest('build/js'))
+       .pipe(rename({suffix: '.min'}))
+       .pipe(uglify())
+       .pipe(gulp.dest('build/js'));
+});
+
 gulp.task('watch', function () {
     gulp.watch('src/scss/**/*.scss', ['styles']);
+    gulp.watch('src/js/**/*.js', ['scripts']);
 });

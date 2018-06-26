8fe9a15aa9e6edcdb6e470b4d84638726fa9c445
danilchican <danilchican@mail.ru>
2018-06-26 17:17:30 +0300

    Styles normalizing task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 const gulp = require('gulp'),
-    uglify = require('gulp-uglify');
+    autoprefixer = require('gulp-autoprefixer'),
+    cleancss = require('gulp-clean-css'),
+    notify = require('gulp-notify'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-ruby-sass');
 
-gulp.task('css', function () {
-    gulp.src('src/scss/main.scss')
-        .pipe(uglify())
-        .pipe(gulp.dest('build/css'));
+gulp.task('styles', function () {
+    return sass('src/scss/main.scss', {style: 'expanded'})
+        .pipe(autoprefixer('last 3 versions'))
+        .pipe(gulp.dest('build/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cleancss({inline: ['none']}))
+        .pipe(gulp.dest('build/css'))
+        .pipe(notify({message: 'Styles task complete'}));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/scss/**/*.scss', ['styles']);
 });
